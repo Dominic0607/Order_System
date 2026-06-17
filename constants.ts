af90efaa1206ff01ae85642ce2bb0aee482c0aa8
rattanakoudom890-oss <rattanakoudom890@gmail.com>
2026-06-17 16:29:33 +0700

    Initial commit

--- constants.ts
+++ constants.ts
@@ -14,7 +14,7 @@ export const LABEL_PRINTER_URL_BASE = "https://samnangh849-source.github.io/Labe
 // APP LOGO URL
 // សូមប្ដូរ Link រូបភាព Logo របស់អ្នកនៅទីនេះ (Google Drive Link ឬ Direct URL)
 // ប្រសិនបើទុកចោល (empty string) កម្មវិធីនឹងប្រើរូបតំណាងលំនាំដើម។
-export const APP_LOGO_URL = "https://drive.google.com/file/d/1vb9IG8wn31LDYtUKLurm6cMow0MI4Tee/view?usp=drive_link";
+export const APP_LOGO_URL = "https://drive.google.com/file/d/1bWpESYbFij2E0PVK4CGml-PoEUV85HsG/view?usp=drive_link";
 
 // ============================================================
 // SOUND LIBRARY - Notification & Status sounds only
